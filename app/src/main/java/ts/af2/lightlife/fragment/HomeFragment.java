9daaad19196e3861dc28e60c26e7b0d2552cc67c
huiym0825 <huiym0825@thundersoft.com>
2017-06-15 18:26:51 +0800

    Add ViewFlipper and other layout

--- app/src/main/java/ts/af2/lightlife/fragment/HomeFragment.java
+++ app/src/main/java/ts/af2/lightlife/fragment/HomeFragment.java
@@ -2,19 +2,23 @@ package ts.af2.lightlife.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 import ts.af2.lightlife.R;
+import ts.af2.lightlife.util.Util;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
+    private ViewFlipper mViewFlipper;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,5 +32,20 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initView();
+    }
 
+    private void initView() {
+        mViewFlipper = (ViewFlipper) getView().findViewById(R.id.home_page_flipper);
+        for (int i = 0; i < Util.flipperImage.length; i++) {
+            ImageView imageView = new ImageView(getActivity());
+            imageView.setImageResource(Util.flipperImage[i]);
+            mViewFlipper.addView(imageView);
+        }
+        mViewFlipper.setAutoStart(true);
+        mViewFlipper.setFlipInterval(2000);
+    }
 }
